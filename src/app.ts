ca1131399e83310077d02eee9bb366f07101fc86
Rafael Dias <rafael.dias@10i9.com.br>
2021-04-15 15:43:52 -0300

    #1(feat) - Create User

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import './config/env.conf';
+import express from 'express';
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+export { app };
